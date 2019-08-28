7101bba1ce7c107fd39ce5d17d398a12c84b8261
mechernyshev <46683231+mechernyshev@users.noreply.github.com>
2019-08-28 14:52:40 +0300

    StopWatch

--- Classroom/Program.cs
+++ Classroom/Program.cs
@@ -23,7 +23,45 @@ namespace Classroom
             //StringUtility.excersise4();
             // StringUtility.excersise5();
 
-            FileUtil.excersice2(@"C:\Windows\System32\drivers\etc\hosts");
+            //FileUtil.excersice2(@"C:\Windows\System32\drivers\etc\hosts");
+
+            //new section
+            PlayingWithStopWatch();
+
+        }
+
+
+        static void PlayingWithStopWatch()
+        {
+            var stopwatch = new StopWatch();
+
+            Console.WriteLine("Experiment #1: one time");
+            stopwatch.Start();
+            System.Threading.Thread.Sleep(1000);
+            stopwatch.Stop();
+
+            stopwatch.ShowTheDuration();
+
+            try
+            {
+                Console.WriteLine("Experiment #2: start several times");
+                stopwatch.Start();
+                stopwatch.Start();
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Oops. something went wrong");
+            }
+           
+
+
+            Console.WriteLine("Experiment #3: stop and start again");
+            stopwatch.Stop();
+            stopwatch.Start();
+            System.Threading.Thread.Sleep(2000);
+            stopwatch.Stop();
+
+            stopwatch.ShowTheDuration();
         }
     }
 
---
+++ Classroom/StopWatch.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classroom
+{
+    class StopWatch
+    {
+
+        private TimeSpan _duration;
+        private DateTime _lastStart;
+        private DateTime _lastStop;
+        private bool _isStartedAlready;
+
+        public StopWatch()
+        {
+            _lastStart = DateTime.Now;
+            _lastStop = DateTime.Now;
+            _isStartedAlready = false;
+        }
+
+
+        public void Start()
+        {
+            if (!_isStartedAlready)
+            {
+                _lastStart = DateTime.Now;
+                _isStartedAlready = true;
+            }
+            else
+            {
+                throw new InvalidOperationException("Stop watch cannot be started multiple times without being stopped");
+            }
+            
+        }
+
+        public void Stop ()
+        {
+            _isStartedAlready = false;
+            _lastStop = DateTime.Now;
+
+            _duration = _lastStop.Subtract(_lastStart);
+        }
+
+        public void ShowTheDuration()
+        {
+            Console.WriteLine("Stop watch shows {0}", _duration);
+        }
+    }
+}
